019cabcb98570e1005c04094a100d3faf7d30ba8
EminJakupovic <jakupovicemin8@gmail.com>
2024-10-25 20:02:16 +0200

    Evo nove lekcije

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Pozdrav iz konzole")
+document.write("Pozz sa stranice")
+//window.alert("Pozz iz popup")
+//console.error("Pozz iz konzole kao eerrrorrerrr")
+document.getElementById("poruka").innerText= "ovo je poruka"
+
+let prvi_broj = 5
+let drugi_broj = "10"
+
+drugi_broj = parseInt(drugi_broj)
+console.log(prvi_broj + drugi_broj)
+
+rezultat = (prvi_broj + drugi_broj)
+let recenica = `Ja sam Emin i imam ${rezultat} godina`;
+console.log(recenica)
